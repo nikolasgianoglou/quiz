f41632b07c796fa4cc6ad2d4a818f2040a7c9f86
Nikolas Gianoglou Coelho <nikolas.coelho@NMAC-5044.local>
2025-05-08 10:44:09 -0300

    improving the game flow with swift generics

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -113,16 +113,16 @@ class FlowTest: XCTestCase {
 
     // MARK: - Helpers
 
-    func makeSUT(questions: [String]) -> Flow {
+    func makeSUT(questions: [String]) -> Flow<String, String, RouterSpy>{
         return Flow(questions: questions, router: router)
     }
 
     class RouterSpy: Router {
         var routedQuestions: [String] = []
         var routedResult: [String: String]? = nil
-        var answerCallback: Router.AnswerCallback = { _ in }
+        var answerCallback: (String) -> Void = { _ in }
 
-        func routeTo(question: String, answerCallback: @escaping Router.AnswerCallback) {
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
             routedQuestions.append(question)
             self.answerCallback = answerCallback
         }
